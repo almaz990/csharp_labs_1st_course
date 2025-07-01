a52f5b80d7d7f7843d8d13ea4cf07c87da23d208
almaz990 <almazz2f@gmail.com>
2025-07-01 19:28:36 +0300

    Add files via upload

---
+++ lab14/Program.cs
@@ -0,0 +1,114 @@
+﻿class Program
+{
+    static void Main()
+    {
+        BankAccount first = new BankAccount(1000, '1');
+        BankAccount second = new BankAccount(1000, '1');
+        BankAccount third = new BankAccount(1001, '1');
+        
+        // 1.
+        
+        Console.WriteLine($"first == second - {first == second}");
+        Console.WriteLine($"first == third - {first == third}");
+        
+        // 2.
+        Console.WriteLine($"GetHashCode: first == second - {first.GetHashCode() == second.GetHashCode()}");
+        Console.WriteLine($"GetHashCode: third == second - {third.GetHashCode() == second.GetHashCode()}");
+
+
+        // 3.
+        Console.WriteLine(first.ToString());
+        Console.WriteLine(second.ToString());
+        Console.WriteLine(third.ToString());
+
+    }
+
+}
+class BankTransaction
+{
+    private readonly DateTime dt;
+    private readonly int summa;
+    public BankTransaction(int summa) { this.summa = summa; this.dt = DateTime.Now; }
+    public string GetTransaction()
+    {
+        return dt.ToString() + "; " + summa.ToString();
+    }
+}
+class BankAccount : IDisposable
+{
+    private readonly int _number = 0;
+    private int _balance = 0;
+    private char _type = '0';
+    private static int _accountNumberCounter = 1000;
+    private Queue<BankTransaction> queue = new Queue<BankTransaction>();
+    private bool _disposed = false;
+    public BankAccount(int balance, char type)
+    {
+        _number = ++_accountNumberCounter;
+        _balance = balance;
+        _type = type;
+    }
+    public BankAccount(int balance) : this(balance, '0') { }
+    public BankAccount(char type) : this(0, type) { }
+    public BankAccount() : this(0, '0') { }
+    public void Withdraw(int money)
+    {
+        if (_disposed)
+            throw new ObjectDisposedException("BankAccount");
+        if (_balance >= money)
+        {
+            _balance -= money;
+            BankTransaction bt = new(money);
+            queue.Enqueue(bt);
+        }
+    }
+    public void Deposit(int money)
+    {
+        if (_disposed)
+            throw new ObjectDisposedException("BankAccount");
+        _balance += money;
+        BankTransaction bt = new(money);
+        queue.Enqueue(bt);
+    }
+    public void GetInfo()
+    {
+        Console.WriteLine($"Info: Number = {_number}; Balance = {_balance}; Type = {_type};\nQueue = {queue.Count}");
+    }
+    public void Dispose()
+    {
+        if (_disposed) return;
+
+        SaveTransactionsToFile();
+        queue.Clear();
+        _disposed = true;
+        GC.SuppressFinalize(this);
+    }
+    private void SaveTransactionsToFile()
+    {
+        string path = @"Transactions.txt";
+        using (StreamWriter sw = new StreamWriter(path, false))
+        {
+            foreach (var tr in queue)
+            { sw.WriteLine(tr.GetTransaction()); }
+        }
+    }
+    public static bool operator== (BankAccount f, BankAccount s)
+    {
+        return f._balance == s._balance && f._type == s._type;
+    }
+    public static bool operator!= (BankAccount f, BankAccount s)
+    {
+        return f._balance != s._balance || f._type != s._type;
+    }
+    public bool Equals(BankAccount obj)
+    {
+        if (obj is null)
+            return false;
+        return this._balance == obj._balance && this._type == obj._type;
+    }
+    public override int GetHashCode() => HashCode.Combine(_balance, _type);
+    public override string ToString()
+    {
+        return $"Info: Number = {_number}; Balance = {_balance}; Type = {_type};\nQueue = {queue.Count}";
+    }
+}
---
+++ lab14/Program1.cs
@@ -0,0 +1,84 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        var books = new[]
+        {
+            new Book("CLR via C#", "Джеффри Рихтер", "Питер"),
+            new Book("Чистый код", "Роберт Мартин", "БХВ"),
+            new Book("Паттерны проектирования", "Эрих Гамма", "Вильямс")
+        };
+        var library = new ContainerBook(books);
+
+        Console.WriteLine("Выберите критерий сортировки: ");
+        var criteria = Console.ReadLine();
+
+        switch(criteria)
+        {
+            case "author": library.SortBooks(BookComparison.CompareAuthor); library.PrintBooks(); break;
+            case "title": library.SortBooks(BookComparison.CompareTitle); library.PrintBooks(); break;
+            case "publisher": library.SortBooks(BookComparison.ComparePublisher); library.PrintBooks(); break;
+            default: Console.WriteLine("Выберите из списка: (author), (title), (publisher)"); break;
+        }
+    }
+}
+
+class Book
+{
+    public string Title { get; }
+    public string Author { get; }
+    public string Publisher { get; }
+
+    public Book(string title, string author, string publisher)
+    {
+        Title = title;
+        Author = author;
+        Publisher = publisher;
+    }
+
+}
+class ContainerBook
+{
+    private Book[] _books;
+    public ContainerBook(Book[] books) { _books = books; }
+    public void SortBooks(CompareBooks cb)
+    {
+        for (int i = 0; i < _books.Length - 1; i++)
+        {
+            for (int j = i + 1; j < _books.Length; j++)
+            {
+                if (cb(_books[i], _books[j]) > 0)
+                {
+                    var t = _books[i];
+                    _books[i] = _books[j];
+                    _books[j] = t;
+                }
+            }
+        }
+    }
+    public void PrintBooks()
+    {
+        foreach (var book in _books)
+        {
+            Console.WriteLine($"{book.Title} | {book.Author} | {book.Publisher}");
+        }
+    }
+}
+class BookComparison
+{
+    public static int CompareTitle(Book f, Book s)
+    {
+        return string.Compare(f.Title, s.Title);
+    }
+    public static int CompareAuthor(Book f, Book s)
+    {
+        return string.Compare(f.Author, s.Author);
+    }
+    public static int ComparePublisher(Book f, Book s)
+    {
+        return string.Compare(f.Publisher, s.Publisher);
+    }
+}
+
+delegate int CompareBooks(Book f, Book s);
