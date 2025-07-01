69618735770acfcf29970e640ca297c4baf57fa3
almaz990 <almazz2f@gmail.com>
2025-07-01 19:13:13 +0300

    Update Program.cs

--- lab01/Program.cs
+++ lab01/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("ex2");
+//1.
+
+Console.WriteLine("1.");
+double n = 5.88;
+Console.WriteLine(n);
+
+//2.
+
+Console.WriteLine("\n2.");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вы ввели число {n1}");
+
+//3.
+
+Console.WriteLine("\n3.");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{n2} вот какое число Вы ввели");
+
+//4.
+
+Console.WriteLine("\n4.");
+Console.WriteLine("1 " + " 13" + " 49");
+
+//5.
+
+Console.WriteLine("\n5.");
+Console.Write("1\n" + "13\n" + "49\n");
+
+//6.
+
+Console.WriteLine("\n6.");
+Console.Write("5 10\n" + "7 см");
